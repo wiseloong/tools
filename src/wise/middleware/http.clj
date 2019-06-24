4ab2024c5f71dd95f796967f87f662bb89cbc4dd
Anyhow-crane <582538746@qq.com>
2019-06-24 16:57:19 +0800

    修复bug 不管其他情况 支持json数据，不支持返回页面数据

--- src/wise/middleware/http.clj
+++ src/wise/middleware/http.clj
@@ -1,21 +1,26 @@
 (ns wise.middleware.http
-  (:require [ring.util.http-response :refer [ok no-content]]))
+  (:require [ring.util.http-response :refer [ok no-content]]
+            [clojure.string :as cstr]))
 
-;; 列表和单个结果不适用，还是要加上ok返回的：(GET "/def" [] (ok "xxx")) (GET "/efg" [] (ok [1 2 3]))
-;; 注意当有这2个接口时：(GET "/abc/bcd" [] {:id 1})  (GET "/abc/:id" [] :path-params [id :- s/Int] (ok {:id id}))
-;; 访问/abc/bcd时，ring匹配的是/abc/:id这个，这样的情况也不适用，第一个接口也要加上(ok {:id 1})
+;; 注意当有这2个接口时：(GET "/abc/bcd" [] nil)  (GET "/abc/:id" [] :path-params [id :- s/Int] (ok {:id id}))
+;; 访问/abc/bcd时，ring匹配的是/abc/:id这个，这样的情况也不适用，第一个接口也要加上(ok nil)
 (defn response-mw
-  "可以省去写返回ok，或no-content，直接返回结果，只支持map类结果
-  1. (GET \"/abc\" [])  2. (GET \"/bcd\" [] {:id 2 :ids [1 2 4 5]})"
+  "可以省去写返回ok，或no-content，直接返回结果
+  1. (GET \"/abc\" [] \"abc\")  2. (GET \"/bcd\" [] {:id 2 :ids [1 2 4 5]})
+  3. (GET \"/def\" [] (list 1 2 3))"
   [handler]
   (fn [request]
-    (let [response (handler request)]
-      (if response
-        (let [body (:body response)]
-          (if (or (nil? body) (= "" body))
+    (if-let [{:keys [status headers body] :as response} (handler request)]
+      (if (= 200 status)
+        (if body
+          (if (= "" body)
             (let [data (dissoc response :status :headers :body)]
               (if (empty? data)
                 (no-content)
                 (ok data)))
-            response))
-        (no-content)))))
+            (if (cstr/includes? (headers "Content-Type") "text/html")
+              (ok body)
+              response))
+          (no-content))
+        response)
+      (no-content))))
